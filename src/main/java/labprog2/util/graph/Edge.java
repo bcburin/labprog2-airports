fbf4913e3455398de6377531beb4686a60177581
bcburin <cburin.bruno@gmail.com>
2022-08-27 00:58:51 -0300

    Added documentation to Edge.java

--- src/main/java/labprog2/util/graph/Edge.java
+++ src/main/java/labprog2/util/graph/Edge.java
@@ -1,40 +1,79 @@
 package labprog2.util.graph;
 
 public class Edge {
-    private Node srcNode;
-    private Node desNode;
+    private final Node srcNode;
+    private final Node desNode;
     private int weight;
 
+    /**
+     * Implements the edge of a graph.
+     *
+     * @param srcNode source node.
+     * @param desNode destiny node.
+     * @param weight weight of the edge.
+     */
     public Edge(Node srcNode, Node desNode, int weight) {
         this.srcNode = srcNode;
         this.desNode = desNode;
         this.weight = weight;
     }
 
+    /**
+     * Implements the edge of a graph. Weight is defaulted to 1.
+     *
+     * @param srcNode source node.
+     * @param desNode destiny node.
+     */
     public Edge(Node srcNode, Node desNode) {
         this.srcNode = srcNode;
         this.desNode = desNode;
         this.weight = 1;
     }
 
+    /**
+     * Copies edge object.
+     *
+     * @param other original edge object.
+     */
     public Edge(Edge other) {
         srcNode = other.getSrcNode();
         desNode = other.getDesNode();
         weight = other.getWeight();
     }
 
+    /**
+     * Weight getter.
+     *
+     * @return weight of the edge.
+     */
     public int getWeight() {
         return weight;
     }
 
+    /**
+     * Source node getter.
+     *
+     * @return source node of the edge.
+     */
     public Node getSrcNode() {
         return srcNode;
     }
 
+    /**
+     * Destiny node getter.
+     *
+     * @return destiny node of the edge.
+     */
     public Node getDesNode() {
         return desNode;
     }
 
+    /**
+     * Setter of the weight.
+     *
+     * @param weight new weight.
+     * @return reference to current edge.
+     */
     public Edge setWeight(int weight) {
         this.weight = weight;
         return this;
